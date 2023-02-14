dc8a232455984e103c39ffd57f3e25250547d5b3
Zarif Mahfuz <mahfuz.zarif@gmail.com>
2023-02-13 21:17:14 -0700

    Initialize the models

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Bike(models.Model):
+    name = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    units_available = models.PositiveIntegerField(default=0)
+    description = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+
+class Customer(models.Model):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+class Sale(models.Model):
+    PAYMENT_CHOICES = (
+        ("cash", "Cash"),
+        ("credit/debit", "Credit/Debit Card")
+    )
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    created_at = models.DateField(auto_now_add=True)
+    payment_method = models.CharField(max_length=50, choices=PAYMENT_CHOICES)
+
+class BikeSale(models.Model):
+    sale = models.ForeignKey(Sale, on_delete=models.PROTECT)
+    bike = models.ForeignKey(Bike, on_delete=models.PROTECT)
+    units = models.PositiveIntegerField(default=1)
+    net_sale = models.DecimalField("sale value - discount applied", max_digits=8, decimal_places=2)
+    refund = models.DecimalField(max_digits=8, decimal_places=2, default=0, blank=True)
+    discount_percentage = models.IntegerField(default=0, blank=True, validators=[
+        MinValueValidator(0), MaxValueValidator(100)
+    ])
