a36acca0c90fe4d078ef7cb369c4a9f650af576a
Zarif Mahfuz <mahfuz.zarif@gmail.com>
2023-02-14 21:43:48 -0700

    Register models on the django admin

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Bike, Customer, BikeSale, Sale
 
 # Register your models here.
+admin.site.register(Bike)
+admin.site.register(Customer)
+admin.site.register(BikeSale)
+admin.site.register(Sale)
--- backend/models.py
+++ backend/models.py
@@ -1,18 +1,21 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+
 class Bike(models.Model):
     name = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     units_available = models.PositiveIntegerField(default=0)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     image = models.ImageField(null=True, blank=True)
+
 
 class Customer(models.Model):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+
 
 class Sale(models.Model):
     PAYMENT_CHOICES = (
@@ -20,15 +23,19 @@ class Sale(models.Model):
         ("credit/debit", "Credit/Debit Card")
     )
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
-    created_at = models.DateField(auto_now_add=True)
+    sold_at = models.DateField()
+    updated_at = models.DateField(auto_now=True)
     payment_method = models.CharField(max_length=50, choices=PAYMENT_CHOICES)
+
 
 class BikeSale(models.Model):
     sale = models.ForeignKey(Sale, on_delete=models.PROTECT)
     bike = models.ForeignKey(Bike, on_delete=models.PROTECT)
     units = models.PositiveIntegerField(default=1)
-    net_sale = models.DecimalField("sale value - discount applied", max_digits=8, decimal_places=2)
-    refund = models.DecimalField(max_digits=8, decimal_places=2, default=0, blank=True)
+    net_sale = models.DecimalField(
+        "net_sale = sale value - discount applied", max_digits=8, decimal_places=2)
+    refund = models.DecimalField(
+        max_digits=8, decimal_places=2, default=0, blank=True)
     discount_percentage = models.IntegerField(default=0, blank=True, validators=[
         MinValueValidator(0), MaxValueValidator(100)
     ])
