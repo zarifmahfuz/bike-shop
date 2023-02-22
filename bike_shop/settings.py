c65b97ef1b2ce292155c1e5a76295758fb60cbaf
Zarif Mahfuz <mahfuz.zarif@gmail.com>
2023-02-22 14:47:20 -0700

    Allow all requests from all origins

--- bike_shop/settings.py
+++ bike_shop/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'backend',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -136,3 +138,5 @@ REST_FRAMEWORK = {
     ],
     'EXCEPTION_HANDLER': 'backend.utils.custom_exception_handler'
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
