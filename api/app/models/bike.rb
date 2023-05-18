1b59240789709ea26613724eaba3bc478a780290
Zarif Mahfuz <mahfuz.zarif@gmail.com>
2023-05-18 15:48:55 -0600

    Add column validations for the Bike model

--- api/app/models/bike.rb
+++ api/app/models/bike.rb
@@ -1,3 +1,6 @@
+require 'uri'
+
 class Bike < ApplicationRecord
-    # TODO: Add validations on columns
+    validates :units_available, numericality: { greater_than_or_equal_to: 0 }
+    validates :image, format: { with: URI::DEFAULT_PARSER.make_regexp, message: "Invalid URL format" }, if: -> { image.present? }
 end
