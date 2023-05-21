bf02453ca4b89bf929bdbf99ab6b20bbb0b49291
Zarif Mahfuz <mahfuz.zarif@gmail.com>
2023-05-21 14:54:49 -0600

    Add column validations on the sale model

--- api/app/models/sale.rb
+++ api/app/models/sale.rb
@@ -1,4 +1,7 @@
 class Sale < ApplicationRecord
-    # TODO: Add column validations
+    validates :payment_method, inclusion: { in: ["cash", "credit/debit"],
+        message: "%{value} is not a valid payment method" }
+    validates :discount_percentage, numericality: { greater_than_or_equal_to: 0, 
+        less_than_or_equal_to: 100, message: "is not in valid range" }
     # TODO: Define associations
 end
