80a5d688b22b91955237416b1c046a41df92da75
Zarif Mahfuz <mahfuz.zarif@gmail.com>
2023-05-21 15:11:35 -0600

    Add the Customer-Sale association

--- api/app/models/customer.rb
+++ api/app/models/customer.rb
@@ -1,3 +1,5 @@
 class Customer < ApplicationRecord
     validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "only allows valid emails" }
+
+    has_many :sales
 end
--- api/app/models/sale.rb
+++ api/app/models/sale.rb
@@ -3,5 +3,6 @@ class Sale < ApplicationRecord
         message: "%{value} is not a valid payment method" }
     validates :discount_percentage, numericality: { greater_than_or_equal_to: 0, 
         less_than_or_equal_to: 100, message: "is not in valid range" }
-    # TODO: Define associations
+
+    belongs_to :customer
 end
